35852d56b8987bb5c0105ffe0a8971892e97404c
Adriano Miranda <72226389+adrianommiranda@users.noreply.github.com>
2025-08-11 17:41:00 -0300

    Criando a classe MarcaProduto e o application.properties simples

---
+++ src/main/java/br/com/miranda/loja_virtual/model/MarcaProduto.java
@@ -0,0 +1,39 @@
+package br.com.miranda.loja_virtual.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table (name = "marca_produto")
+@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 0)
+public class MarcaProduto implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+    private Long id;
+    private  String nomeDesc;
+
+    public MarcaProduto(Long id, String nomeDesc) {
+        this.id = id;
+        this.nomeDesc = nomeDesc;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeDesc() {
+        return nomeDesc;
+    }
+
+    public void setNomeDesc(String nomeDesc) {
+        this.nomeDesc = nomeDesc;
+    }
+}
