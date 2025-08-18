7c3d6367f041eee01c517ea0070ee38035920951
Adriano Miranda <72226389+adrianommiranda@users.noreply.github.com>
2025-08-18 18:10:36 -0300

    Cupom de desconto, Imagem do Produto, Nota Fiscal de Compra, Nota Item do produto, Produto

---
+++ src/main/java/br/com/miranda/loja_virtual/model/CupomDesconto.java
@@ -0,0 +1,79 @@
+package br.com.miranda.loja_virtual.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "cupom_desconto")
+@SequenceGenerator(name = "seq_cupom_desconto", sequenceName = "seq_cupom_desconto", allocationSize = 1, initialValue = 1)
+public class CupomDesconto implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_cupom_desconto")
+    private Long id;
+
+    private String descricao;
+    private BigDecimal valorRealDesc;
+    private BigDecimal valorPorcentoDesc;
+
+    @Temporal(TemporalType.DATE)
+    private Date dataValidadeCupom;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getValorRealDesc() {
+        return valorRealDesc;
+    }
+
+    public void setValorRealDesc(BigDecimal valorRealDesc) {
+        this.valorRealDesc = valorRealDesc;
+    }
+
+    public BigDecimal getValorPorcentoDesc() {
+        return valorPorcentoDesc;
+    }
+
+    public void setValorPorcentoDesc(BigDecimal valorPorcentoDesc) {
+        this.valorPorcentoDesc = valorPorcentoDesc;
+    }
+
+    public Date getDataValidadeCupom() {
+        return dataValidadeCupom;
+    }
+
+    public void setDataValidadeCupom(Date dataValidadeCupom) {
+        this.dataValidadeCupom = dataValidadeCupom;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        CupomDesconto that = (CupomDesconto) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
